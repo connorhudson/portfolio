10f892f65075d0a570ef21542d5a7e6d8f6fef71
Connor Hudson <cohu3291@colorado.edu>
2019-01-29 19:41:38 -0700

    Fix model export/imports

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,5 +1,10 @@
+/**
+ * Enum of all categories in the app
+ */
 enum Category {
     PERSONAL = 'personal',
     PROFESSIONAL = 'professional',
     SCHOOL = 'school'
 }
+
+export default Category
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,5 +1,7 @@
+import Category from "./Category";
+
 /**
- * Format for all projects listed on 
+ * Data format for all projects displayed on portfolio
  */
 interface Project {
     category: Category
@@ -10,3 +12,5 @@ interface Project {
     images: Array<{url: URL, alt: string}>, // images to display in the project section
     processLink?: URL // link to a blog post on process.glitch.pizza, if available
 }
+
+export default Project
