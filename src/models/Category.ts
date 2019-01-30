0e7351e3c7bdf22e18d7ab96cc748512e334ab3a
Connor Hudson <cohu3291@colorado.edu>
2019-01-29 19:14:22 -0700

    Add models for projects

---
+++ src/models/Category.ts
@@ -0,0 +1,5 @@
+enum Category {
+    PERSONAL = 'personal',
+    PROFESSIONAL = 'professional',
+    SCHOOL = 'school'
+}
---
+++ src/models/Project.ts
@@ -0,0 +1,12 @@
+/**
+ * Format for all projects listed on 
+ */
+interface Project {
+    category: Category
+    title: string, // Name of the project
+    logo?: URL, // URL for an image to show in place of a title (i.e. the FORECAST logo)
+    date: string, // TODO: is there a typescript way to specify datetime string?
+    description: string, // TODO: consider making this support rich text/markdown
+    images: Array<{url: URL, alt: string}>, // images to display in the project section
+    processLink?: URL // link to a blog post on process.glitch.pizza, if available
+}
